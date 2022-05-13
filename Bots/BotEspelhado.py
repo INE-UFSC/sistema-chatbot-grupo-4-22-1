05beefeae7e65a00a3de6b00c8dce17aad4ee95e
arrudabea <tobernardo.arrs@gmail.com>
2022-05-13 11:17:27 -0300

    adicionando comandos no bot espelhado

--- Bots/BotEspelhado.py
+++ Bots/BotEspelhado.py
@@ -1,36 +1,38 @@
 from codeop import CommandCompiler
 from Bots.Bot import Bot
 
+
 class BotEspelhado(Bot):
-    def __init__(self,nome):
+    def __init__(self, nome):
         self.__nome = nome
         super().comandos = {
-            '1' : Comando(1, "")
-            '2' : Comando
-
+            "1": ("Bom Dia", "")
+            "2": ("Qual o seu nome?", "")
+            "3": ("Quero um conselho", "")
+            "4": ("Adeus", "")
         }
 
-    #nao esquecer o decorator
+    # nao esquecer o decorator
     @property
     def nome(self):
         return self.__nome
 
-    #nao esquecer o decorator
+    # nao esquecer o decorator
     @nome.setter
-    def nome(nome):
-        pass
-    
+    def nome(self, nome):
+        self.__nome = nome
+
     def apresentacao(self):
-        pass
- 
+        return "...odahlepse é olaf ue euq o odut ,odahlepsE toB o uos rorriM é emon uem ,álO" #fazer o nome ser printado utilizando o self.__nome espelhado
+
     def mostra_comandos(self):
-        pass
-    
-    def executa_comando(self,cmd):
+        return self.comandos
+
+    def executa_comando(self, cmd):
         pass
 
     def boas_vindas(self):
-        pass
+        return "...odnalaf uotse euq o rednetne arap amelborp ahnet oãn euq orepse ,uehlocse em êcov abO"
 
     def despedida(self):
-        pass
+        return ""
