a986ec682596ec2c453f9a11ba885953a54ff6c5
arrudabea <tobernardo.arrs@gmail.com>
2022-05-20 09:41:05 -0300

    adicionando o botcachorro do grupo 6

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 from SistemaChatBot import SistemaChatBot as scb
 from Bots.BotSolitario import BotSolitario
 from Bots.BotEspelhado import BotEspelhado
+from Bots.BotCachorro import BotCachorro
 
 # construa a lista de bots disponíveis aqui
 lista_bots = [BotSolitario("Sozinho"), BotEspelhado("Mirror")]
